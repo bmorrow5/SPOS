e86b6c75fbdfa8a79c45ac47e597d07e581576ae
bmorrow5 <brandonmorrow09@gmail.com>
2024-04-12 18:04:43 -0400

    Update Bayesian Games Method Complete

--- backend/data_access/data_access.py
+++ backend/data_access/data_access.py
@@ -48,6 +48,76 @@ class DataAccess:
         return json.dumps(data, default=str)
 
 
+    def update_bayesian_game(self, game_id, buyer_power=None, seller_power=None, buyer_reservation_price=None, seller_reservation_price=None, current_strategy=None):
+        """Returns the last offer price and the game information.
+        """
+        query = []
+        params = []
+
+        if buyer_power is not None:
+            query.append("buyer_power = %s")
+            params.append(buyer_power)
+        
+        if seller_power is not None:
+            query.append("seller_power = %s")
+            params.append(seller_power)
+        
+        if buyer_reservation_price is not None:
+            query.append("buyer_reservation_price = %s")
+            params.append(buyer_reservation_price)
+        
+        if seller_reservation_price is not None:
+            query.append("seller_reservation_price = %s")
+            params.append(seller_reservation_price)
+        
+        if current_strategy is not None:
+            query.append("current_strategy = %s")
+            params.append(current_strategy)
+
+        # If no updates provided
+        if not query:
+            return json.dumps({'message': 'No updates provided'})
+
+        query = ', '.join(query)
+        params.append(game_id)
+        sql_query = f"UPDATE spos.games SET {query} WHERE game_id = %s;"# Parameterized %s prevents SQL injection. This should still be safe even inserting query, no user inputs inserted
+
+
+        try:
+            with psycopg2.connect(**self.connection_params) as conn:
+                with conn.cursor() as cur:
+                    cur.execute(sql_query, tuple(params))
+
+                    # Check if any rows were returned
+                    if cur.rowcount == 0:
+                        return json.dumps({'message': 'No data found for this game ID'})
+                    
+                    # Confirm changes
+                    cur.execute("SELECT * FROM spos.games WHERE game_id = %s", (game_id,))
+                    updated_row = cur.fetchall()
+                    # Unpack values
+                    (game_id, seller_id, buyer_agent_id, product_id, buyer_power, seller_power, buyer_reservation_price, seller_reservation_price, current_strategy) = updated_row[0]
+        except Exception as e:
+            return json.dumps({'message': 'Error connecting to the database', 'error': str(e)})
+
+        # Label values
+        dict = {'game_id': game_id,
+                'seller_id': seller_id,
+                'buyer_agent_id': buyer_agent_id,
+                'product_id': product_id,
+                'buyer_power': buyer_power,
+                'seller_power': seller_power,
+                'buyer_reservation_price': buyer_reservation_price,
+                'seller_reservation_price': seller_reservation_price,
+                'current_strategy': current_strategy,
+                }
+        data = {'message': 'Data updated successfully', 'data': dict}
+
+        return json.dumps(data, default=str)
+
+
+
+
 
 
 
@@ -111,6 +181,10 @@ if __name__ == "__main__":
 
     # Testing
     da = DataAccess()
-    game_data = da.get_bayesian_game(game_id=1)
-    game_data = json.loads(game_data)
-    print(game_data['data'])
+    # game_data = da.get_bayesian_game(game_id=1)
+    # game_data = json.loads(game_data)
+    # print(game_data['data'])
+
+    data = da.update_bayesian_game(game_id=1, buyer_reservation_price= 10)
+    update_data = json.loads(data)
+    print(update_data)
