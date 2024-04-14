585b7a30ee2a10147a1f0c9c90d664e16137530a
bmorrow5 <brandonmorrow09@gmail.com>
2024-04-14 17:46:17 -0400

    Request quotes working!

--- backend/email_service/email_service.py
+++ backend/email_service/email_service.py
@@ -7,12 +7,108 @@ from backend.data_service.data_service import DataService
 
 
 class EmailService():
-    def __init__(self, email, password):
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
         self.smtp_server = "smtp.gmail.com"  
         self.smtp_port = 587  
         self.imap_server = "imap.gmail.com" 
+
+
+
+
+    def send_email(self, to_email: list, subject: str, message: str) -> str:
+        """ This will send an email
+        """
+        try:
+            if subject is None:
+                subject = "No Subject"
+            if message is None:
+                message = "No Message"
+
+            for email in to_email:
+                # Setup the email
+                msg = MIMEMultipart()
+                msg['From'] = self.email
+                msg['To'] = email
+                msg['Subject'] = subject
+                msg.attach(MIMEText(message, 'plain'))
+                with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
+                    server.starttls()
+                    server.login(self.email, self.password)
+                    text = msg.as_string()
+                    server.sendmail(self.email, email, text)
+                return True
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+            return False
+        
+
+
+    def request_quotes(self, product, subject=None, message=None) -> bool:
+        """Emails to request initial quotes
+       Args:
+            email_list (list): Suppliers to send email to
+            counter_offer_price (float): Bayesian Games counter_offer_price
+            subject (str): Email subject
+            message (str): Email contents
+            Product (Product): Product to request quotes for
+
+        Returns:
+            bool: True if emails sent, False if failed
+        """
+        try:
+            ds = DataService()
+            sellers = ds.read_all_sellers()
+            if subject is None:
+                subject = "Request for Quote"
+            
+            # If message is none make a custom message for each seller
+            if message is None:
+                message = f"To Whom It May Concern,\n\nWe are requesting a quote for the following product: {product.name} in the quanity of {product.quantity}.\n\nPlease reply to this email with your best price, and in the body of the email not on an attachment. \n\nThank you,\n\n{self.first_name} {self.last_name}\nYour Company Intl."
+            
+            for seller in sellers:
+                EmailService.send_email(self, [seller.email], subject, message)
+            return True
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+            return False
+
+
+    def check_email(self):
+        try:
+            # Connect to IMAP server
+            mail = imaplib.IMAP4_SSL(self.imap_server)
+            mail.login(self.email, self.password)
+            mail.select('inbox')  # Connect to inbox
+
+            # Search for all emails
+            status, messages = mail.search(None, 'ALL')
+            if status != 'OK':
+                print("No emails found!")
+                return
+
+            # Process emails
+            for num in messages[0].split():
+                status, data = mail.fetch(num, '(RFC822)')
+                if status != 'OK':
+                    print("ERROR getting message", num)
+                    return
+
+                # Parse email content
+                msg = email.message_from_bytes(data[0][1])
+                print('From:', msg['From'])
+                print('Subject:', msg['Subject'])
+                print("Message:", msg.get_payload(decode=True))
+
+            mail.close()
+            mail.logout()
+        except Exception as e:
+            print(f"Failed to read emails: {e}")
+
+
 
 
     def send_counteroffer(self, from_email: str, to_email: list, counter_offer_price: float, message: str, original_message_id: str) -> str:
@@ -55,77 +151,3 @@ class EmailService():
                 print("Email sent successfully!")
         except Exception as e:
             print(f"Failed to send email: {e}")
-
-    def request_quotes(self, subject: str, message: str, Product: object) -> str:
-        """Emails to request initial quotes
-       Args:
-            email_list (list): Suppliers to send email to
-            counter_offer_price (float): Bayesian Games counter_offer_price
-            subject (str): Email subject
-            message (str): Email contents
-            Product (Product): Product to request quotes for
-
-        Returns:
-            str: confirmation email was sent
-        """
-        
-        try:
-            ds = DataService()
-            sellers = ds.read_all_sellers()
-            if subject is None:
-                subject = "Request for Quote"
-            if message is None:
-                message = f"We are requesting a quote for the following product: {Product.name} in the quanity of {Product.quantity}"
-
-            for seller in sellers:
-                # Setup the email
-                msg = MIMEMultipart()
-                msg['From'] = self.email
-                msg['To'] = seller.email
-                msg['Subject'] = subject
-                msg.attach(MIMEText(message, 'plain'))
-
-                # Connect to the server and send the email
-                server = smtplib.SMTP(self.smtp_server, self.smtp_port)
-                server.starttls()
-                server.login(self.email, self.password)
-                text = msg.as_string()
-                server.sendmail(self.email, seller.email, text)
-                server.quit()
-                return True
-        except Exception as e:
-            print(f"Failed to send email: {e}")
-            return False
-
-
-
-    def check_email_for_offers(self):
-        try:
-            # Connect to IMAP server
-            mail = imaplib.IMAP4_SSL(self.imap_server)
-            mail.login(self.email, self.password)
-            mail.select('inbox')  # Connect to inbox
-
-            # Search for all emails
-            status, messages = mail.search(None, 'ALL')
-            if status != 'OK':
-                print("No emails found!")
-                return
-
-            # Process emails
-            for num in messages[0].split():
-                status, data = mail.fetch(num, '(RFC822)')
-                if status != 'OK':
-                    print("ERROR getting message", num)
-                    return
-
-                # Parse email content
-                msg = email.message_from_bytes(data[0][1])
-                print('From:', msg['From'])
-                print('Subject:', msg['Subject'])
-                print("Message:", msg.get_payload(decode=True))
-
-            mail.close()
-            mail.logout()
-        except Exception as e:
-            print(f"Failed to read emails: {e}")
--- tests/test_email_service.py
+++ tests/test_email_service.py
@@ -4,25 +4,23 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(1, parent_dir)
 import unittest
-from unittest.mock import patch, MagicMock
 from backend.email_service.email_service import EmailService
 from backend.data_service.data_service import DataService
 from backend.data_service.models import ProductDatabase
 
 class TestEmailService(unittest.TestCase):
     def setUp(self):
-        self.email_service = EmailService('spos6045@gmail.com', 'cjoisegsetxkqdxb')
+        self.email_service = EmailService(first_name= "John", last_name="Doe", email='spos6045@gmail.com', password='cjoisegsetxkqdxb')
 
-    @patch('smtplib.SMTP')
-    def test_request_quotes(self, mock_smtp):        
+    def test_request_quotes(self):        
         # New product
         product = ProductDatabase(name="Office Chairs", quantity= 100.0, max_price= 100.0, date_needed_by= "2021-12-12")
 
         # Call the method
-        result = self.email_service.request_quotes('subject', 'message', product)
+        result = self.email_service.request_quotes(product=product)
 
         # Asserts
-        self.assertEqual(result, True)
+        self.assertTrue(result)
 
 if __name__ == '__main__':
     unittest.main()
