055d49e88a51f3c167052d120679fe009af98072
bmorrow5 <brandonmorrow09@gmail.com>
2024-04-14 14:54:42 -0400

    Fixed Import Error

--- backend/data_service/data_service.py
+++ backend/data_service/data_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
-from models import SellerDatabase, BuyerAgentDatabase, ProductDatabase, GameDatabase, EmailLogDatabase
+from .models import Base
+from .models import SellerDatabase, BuyerAgentDatabase, ProductDatabase, GameDatabase, EmailLogDatabase
 from sqlalchemy.exc import SQLAlchemyError
 import hashlib
 import logging
--- tests/test_email_service.py
+++ tests/test_email_service.py
@@ -1,10 +1,15 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(1, parent_dir)
 import unittest
 from unittest.mock import patch, MagicMock
-from backend.email_service.email_service_test import EmailService
+from backend.email_service.email_service import EmailService
 
 class TestEmailService(unittest.TestCase):
     def setUp(self):
-        self.email_service = EmailService('example@gmail.com', 'password123')
+        self.email_service = EmailService('spos6045@gmail.com', 'cjoisegsetxkqdxb')
 
     @patch('smtplib.SMTP')
     def test_send_counteroffer_success(self, mock_smtp):
@@ -13,12 +18,12 @@ class TestEmailService(unittest.TestCase):
         mock_smtp.return_value = server_mock
         
         # Call the method
-        result = self.email_service.send_counteroffer('example@gmail.com', ['receiver@example.com'], 100.0, 'Please consider our counteroffer.', 'original-msg-id')
+        result = self.email_service.send_counteroffer('spos6045@gmail.com', ['brandonmorrow09@gmail.com'], 100.0, 'Please consider our counteroffer.', 'original-msg-id')
 
         # Asserts
         self.assertEqual(result, "Email sent successfully!")
         server_mock.starttls.assert_called_once()
-        server_mock.login.assert_called_once_with('example@gmail.com', 'password123')
+        server_mock.login.assert_called_once_with('spos6045@gmail.com', 'cjoisegsetxkqdxb')
         server_mock.sendmail.assert_called_once()
         server_mock.quit.assert_called_once()
 
@@ -31,7 +36,7 @@ class TestEmailService(unittest.TestCase):
         mock_smtp.return_value = server_mock
 
         # Call the method
-        result = self.email_service.send_counteroffer('example@gmail.com', ['receiver@example.com'], 100.0, 'Please consider our counteroffer.', 'original-msg-id')
+        result = self.email_service.send_counteroffer('spos6045@gmail.com', ['brandonmorrow09@gmail.com'], 100.0, 'Please consider our counteroffer.', 'original-msg-id')
 
         # Asserts
         self.assertIn("Failed to send email:", result)
