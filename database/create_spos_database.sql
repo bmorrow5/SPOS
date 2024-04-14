d3a988baaa126664e1d7322532883c7a74053795
bmorrow5 <brandonmorrow09@gmail.com>
2024-04-13 23:41:24 -0400

    SQL Updated correctly and database working

--- database/create_spos_database.sql
+++ database/create_spos_database.sql
@@ -6,20 +6,6 @@
 CREATE SCHEMA IF NOT EXISTS spos
     AUTHORIZATION postgres;
 
--- Email storage
-CREATE TABLE IF NOT EXISTS spos.email_logs (
-    email_log_id SERIAL PRIMARY KEY,
-    sender_email VARCHAR(255) NOT NULL,
-    receiver_email VARCHAR(255) NOT NULL,
-    buyer_agent_id INT,
-    seller_id INT,
-    subject TEXT NOT NULL,
-    body TEXT NOT NULL,
-    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (buyer_agent_id) REFERENCES spos.buyer_agents(buyer_agent_id),
-    FOREIGN KEY (seller_id) REFERENCES spos.sellers(seller_id)
-);
-
 -- Players and products
 CREATE TABLE IF NOT EXISTS spos.sellers (
     seller_id SERIAL PRIMARY KEY,
@@ -61,20 +47,30 @@ CREATE TABLE IF NOT EXISTS spos.games (
     FOREIGN KEY (seller_id) REFERENCES spos.sellers(seller_id),
     FOREIGN KEY (product_id) REFERENCES spos.products(product_id)
 );
-
+-- Email storage
+CREATE TABLE IF NOT EXISTS spos.email_logs (
+    email_log_id SERIAL PRIMARY KEY,
+    sender_email VARCHAR(255) NOT NULL,
+    receiver_email VARCHAR(255) NOT NULL,
+    buyer_agent_id INT,
+    seller_id INT,
+    subject TEXT NOT NULL,
+    body TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (buyer_agent_id) REFERENCES spos.buyer_agents(buyer_agent_id),
+    FOREIGN KEY (seller_id) REFERENCES spos.sellers(seller_id)
+);
 
 -- Insert test data
 
--- Insert test data into email_logs
-INSERT INTO spos.email_logs (sender_email, receiver_email, subject, body)
-VALUES ('spos6045@gmail.com', 'brandonmorrow09@gmail.com', 1, 1, 'Test Email Subject', 'This is a test email body.');
+
 
 -- Insert test data into sellers
-INSERT INTO spos.sellers (name, email)
+INSERT INTO spos.sellers (first_name, last_name, email)
 VALUES ('Brandon', 'Morrow', 'brandonmorrow09@gmail.com');
 
 -- Insert test data into buyer_agents
-INSERT INTO spos.buyer_agents (name, employee_id, email, password)
+INSERT INTO spos.buyer_agents (first_name, last_name, employee_id, email, password)
 VALUES ('John', 'Doe', 1011001, 'spos6045@gmail.com', 'encrypted_password');
 
 -- Insert test data into products
@@ -84,3 +80,7 @@ VALUES (1, 1, 100, 50.0, '2023-12-31');
 -- Insert test data into games
 INSERT INTO spos.games (seller_id, buyer_agent_id, product_id, buyer_power, seller_power, buyer_reservation_price, seller_reservation_price, current_strategy)
 VALUES (1, 1, 1, 50, 50, 45.0, 55.0, 'conciliatory'); 
+
+-- Insert test data into email_logs
+INSERT INTO spos.email_logs (sender_email, receiver_email, buyer_agent_id, seller_id, subject, body)
+VALUES ('spos6045@gmail.com', 'brandonmorrow09@gmail.com', 1, 1, 'Test Email Subject', 'This is a test email body.');
